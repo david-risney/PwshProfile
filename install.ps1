40e1f4b1bb77595624c08486f502e6f526bf5a5d
Dave Risney <dave@deletethis.net>
2022-05-07 11:52:37 -0700

    Fixup install script

--- install.ps1
+++ install.ps1
@@ -1,3 +1,13 @@
-New-Item -ItemType HardLink -Name ~\profile.ps1 -Value $PSScriptRoot\profile.ps1
-New-Item -ItemType HardLink -Name ~\TerminalClickable.format.ps1xml -Value $PSScriptRoot\TerminalClickable.format.ps1xml
-New-Item -ItemType HardLink -Name ~\oh-my-posh.json -Value $PSScriptRoot\oh-my-posh.json
+$profilePath = (Get-Item ~).FullName;
+$paths = @("profile.ps1", "TerminalClickable.format.ps1xml", "oh-my-posh.json");
+
+$paths | ForEach-Object {
+    $path = $_;
+    $targetPath = (Join-Path $profilePath $path);
+    $sourcePath = (Join-Path $PSScriptRoot $path);
+    New-Item -Force -ItemType HardLink -Path $targetPath -Value $sourcePath;
+}
+
+if (gc $profile | ?{ $_ -like ". ~\profile.ps1" }) {
+    "`n. ~\profile.ps1" >> $profile;
+}
