1f1afa996346c31329371aab6dce9eb05db062ef
Dave Risney <davris@ntdev.microsoft.com>
2023-02-17 16:19:56 -0800

    Include dependency on powershell and git as comments

--- install.ps1
+++ install.ps1
@@ -1,3 +1,6 @@
+# winget install --id Microsoft.Powershell --source winget # install latest PowerShell
+# winget install git --source winget # install git
+
 $profilePath = (Get-Item ~).FullName;
 $paths = @("profile.ps1", "TerminalClickable.format.ps1xml", "oh-my-posh.json");
 
