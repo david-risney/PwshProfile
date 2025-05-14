5785250a7de09ad46e5fb340477e1047777e0bee
Dave Risney <davris@ntdev.microsoft.com>
2025-05-14 15:09:38 -0700

    Update tasks

--- Get-AutoNinjaLog.ps1
+++ Get-AutoNinjaLog.ps1
@@ -4,7 +4,13 @@ param(
     [switch] $Watch);
 
 # find the most recently run folder under gitRoot\out
-$buildLogPath = (Get-ChildItem @("$Path\out\*\siso_output","$Path\out\*\build.log") -File -ErrorAction Ignore | Sort-Object LastWriteTime -Descending | Select-Object -First 1).FullName;
+$buildLogPath = (Get-ChildItem @(
+        "$Path\out\*\siso_output",
+        "$Path\out\*\build.log") -File -ErrorAction Ignore | 
+        Sort-Object LastWriteTime -Descending | 
+        Select-Object -First 1
+    ).FullName;
+
 Write-Verbose "BuildLogPath: $buildLogPath";
 
 if (!(Test-Path $buildLogPath)) {
@@ -15,6 +21,19 @@ if (!(Test-Path $buildLogPath)) {
     if (!($Watch)) {
         findstr "error info warning note" "$buildLogPath"
     } else {
-        Get-Content $buildLogPath -Wait;
+        $previousDate = $null;
+        $currentDate = (Get-ChildItem $buildLogPath).LastWriteTime;
+        do {
+            if ($previousDate -ne $currentDate) {
+                $previousDate = $currentDate;
+                $currentDate = (Get-ChildItem $buildLogPath).LastWriteTime;
+
+                "---START LOG note---";
+                Get-Content $buildLogPath;
+                "---END LOG note---";
+            } else {
+                Start-Sleep -Seconds 1;
+            }
+        } while ($true);
     }
 }
